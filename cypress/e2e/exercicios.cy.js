ef1ad75170ed3f2fc97b5fd527ec9b28f36156da
WillianRRP <willianrenato754@gmail.com>
2025-05-10 00:03:01 -0300

    create cypress

---
+++ cypress/e2e/exercicios.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('exercicio', () => {
+    
+  it('deve carregar a página corretamente e clicar no botão ‘Ver pets disponíveis para adoção', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Ver pets disponíveis para adoção').click()
+  })
+  it('Visita a página de principal do AdoPet e testa os botão de home', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Ver pets disponíveis para adoção').click()
+  })
+  it('Visita a página de login', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/login');
+  })
+    it("Visita a página de /home do Adopet", ()=>{
+        cy.visit('https://adopet-frontend-cypress.vercel.app/home');
+    })
+    it("Visita a página de /home do AdoPet e clique no botão “Falar com o responsável”", ()=>{
+        cy.visit('https://adopet-frontend-cypress.vercel.app/home');
+        cy.get('.header__message').click()  
+    })
+})
---
+++ roteiro.md
@@ -0,0 +1,18 @@
+Funionalidade: Cadastro no site Adopet
+
+Cenaário: Realizar um cadastro no site com sucesso
+
+Passos:
+O Usuário acessa a página de cadastro.
+O Usuário preenche o campo "nome"
+O Usuário preenche o campo "E-mail" com um E-mail válido.
+O Usuário preenche o campo "Senha" com uma senha válida.
+O Usuário preenche o campo "Confirmação de Senha"  com a mesma senha inserida no passo anterior.
+
+Resultados esperados:
+
+o sistema processa as informações fornecidas
+
+Regra de negócio:
+
+E-mail e senha são campos obrigatórios para cadastro
